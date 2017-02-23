65f35666ae49bbaaf72295d1568d7af61facad4c
PLeon6822 <p.blobest@gmail.com>
2017-02-23 21:25:05 +0100

    Method pondera added

--- submission.py
+++ submission.py
@@ -11,14 +11,17 @@ class Endpoint(object):
         super(Endpoint, self).__init__()
         self.dc_latency = dc
         self.cache_latencies = cache
-        self.video_requests = []
+        self.video_requests = {}
 
     def set_video(self, videos):
         self.video_requests = videos
 
-    def pondera(cache):
-
-        return
+    def pondera(self, cache):
+        # Pondera cada video usando las latencias de cada cache y las requests de los videos para el endpoint #
+        dictionary = {}
+        for video in self.video_requests:
+            dictionary[video] = float(self.video_requests[video])/float(self.cache_latencies[cache])
+        return dictionary
 
 class Cache(object):
     """docstring for Cache."""
@@ -27,6 +30,10 @@ class Cache(object):
         self.videos_size = v
 
 def main():
+    e = Endpoint(1000, {0:200, 1:300})
+    e.video_requests[0] = 1000
+    e.video_requests[1] = 2000
+    print e.pondera(1)
 
 
 if __name__ == "__main__":
